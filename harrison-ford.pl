88a44b489d8d4facbcd486eeb800c72211472d89
Patrick M. Niedzielski <patrick@pniedzielski.net>
2020-01-10 03:37:23 +0000

    Add Cap'n Crunch mode
    This mode is entered with the flag `--capn-crunch`.
    
        italic:harrison-ford(0)$ harrison-ford.pl --capn-crunch
        Unthinkable Tragedy: Cap’n Crunch cereal recalled due to possible salmonella
    
    License: CC0
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>

--- harrison-ford.pl
+++ harrison-ford.pl
@@ -1,7 +1,15 @@
 #!/usr/bin/perl
 use HTML::Entities;
 
-my $doc = `curl -L -s "https://news.google.com/search?q=harrison+ford&hl=en-US&gl=US&ceid=US:en"`;
+my $capn_mode = $ARGV[0] eq '--capn-crunch';
+
+my $doc;
+
+if ($capn_mode) {
+    $doc = `curl -L -s "https://news.google.com/search?q=cap'n+crunch&hl=en-US&gl=US&ceid=US:en"`;
+} else {
+    $doc = `curl -L -s "https://news.google.com/search?q=harrison+ford&hl=en-US&gl=US&ceid=US:en"`;
+}
 
 my @titles = $doc =~ m| class="DY5T1d" >(.*?)</a>|g;
 shift @titles;  shift @titles;
@@ -9,4 +17,25 @@ shift @titles;  shift @titles;
 my $index = int(rand($#titles + 1));
 my $title = decode_entities($titles[$index]);
 $title =~ s/- .+?$//;
-print "Lucky To Be Alive: $title\n";
+
+my $prefix;
+
+if ($capn_mode) {
+    my @prefixes = ( 'Fascinating',
+                     'Unthinkable Tragedy',
+                     'TIL',
+                     'Where Would We Be Without Him',
+                     'Outstanding',
+                     'Who Knew?',
+                     'A Happy Ending',
+                     'He\'s No Harrison Ford',
+                     'This Is Everything That\'s Wrong With America',
+                     'Lucky Charms Take Note',
+                     'The Left Has A New Scapegoat'
+                   );
+    $prefix = $prefixes[rand @prefixes];
+} else {
+    $prefix = "Lucky To Be Alive";
+}
+
+print "$prefix: $title\n";
